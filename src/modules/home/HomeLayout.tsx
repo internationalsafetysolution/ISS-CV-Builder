c5b38ba84df00495285710c7a6f9647f95807e7a
internationalsafetysolution <intsafetysolution@gmail.com>
2025-04-11 23:05:19 +0500

    Add files via upload

--- src/modules/home/HomeLayout.tsx
+++ src/modules/home/HomeLayout.tsx
@@ -57,7 +57,7 @@ const HomeLayout = () => {
                 </div>
 
                 <div className="flex md:hidden">
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-[#4E9D48] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-[#4E9D48] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                     ) : (
