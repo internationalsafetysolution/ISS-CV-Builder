7370186bd5dc2409fb995ff58b864a96e64c870f
internationalsafetysolution <intsafetysolution@gmail.com>
2025-04-11 22:41:14 +0500

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { GLOBAL_MUI_THEME } from '../styles/global.theme';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+import { Analytics } from '@vercel/analytics/react'; // ✅ ADD THIS
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={GLOBAL_MUI_THEME}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Component {...pageProps} />
+          <>
+            <Component {...pageProps} />
+            <Analytics /> {/* ✅ ADD THIS */}
+          </>
         </LocalizationProvider>
       </ThemeProvider>
     </StyledEngineProvider>
