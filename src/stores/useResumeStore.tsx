744aec16c10d5af50d1c918d25433aaf802cefc2
internationalsafetysolution <intsafetysolution@gmail.com>
2025-04-18 14:22:56 +0500

    Add files via upload

--- src/stores/useResumeStore.tsx
+++ src/stores/useResumeStore.tsx
@@ -1,13 +1,7 @@
 import {
-  useDatabases,
-  useFrameworks,
   useLanguages,
-  useLibraries,
-  usePractices,
   useTechnologies,
-  useTools,
-} from '@/stores/skills';
-
+} from '@/stores/skills'; // Removed the unused imports
 import ResumeData from '@/helpers/constants/resume-data.json';
 import { useActivity } from './activity';
 import { useAwards } from './awards';
@@ -26,12 +20,7 @@ export const useResumeStore = () => {
     volunteer: useVoluteeringStore((state) => state.volunteeredExps),
     skills: {
       languages: useLanguages((state) => state.values),
-      frameworks: useFrameworks((state) => state.values),
       technologies: useTechnologies((state) => state.values),
-      libraries: useLibraries((state) => state.values),
-      databases: useDatabases((state) => state.values),
-      practices: usePractices((state) => state.values),
-      tools: useTools((state) => state.values),
     },
     activities: useActivity((state) => state.get()),
   };
@@ -43,12 +32,7 @@ export const useResumeStore = () => {
 export const resetResumeStore = () => {
   useBasicDetails.getState().reset(ResumeData.basics);
   useLanguages.getState().reset(ResumeData.skills.languages);
-  useFrameworks.getState().reset(ResumeData.skills.frameworks);
-  useLibraries.getState().reset(ResumeData.skills.libraries);
-  useDatabases.getState().reset(ResumeData.skills.databases);
   useTechnologies.getState().reset(ResumeData.skills.technologies);
-  usePractices.getState().reset(ResumeData.skills.practices);
-  useTools.getState().reset(ResumeData.skills.tools);
   useExperiences.getState().reset(ResumeData.work);
   useEducations.getState().reset(ResumeData.education);
   useVoluteeringStore.getState().reset(ResumeData.volunteer);
